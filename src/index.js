27daa0ca3751380393dcfd520790562faf169474
Sean Huang <hyprul@ucla.edu>
2019-11-27 16:21:02 -0600

    jsxxxxx

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const lucky_number = 7;
 ReactDOM.render(
   <div>
     <h1>Hello {name}!</h1>
-    <p>Your lucky number is {lucky_number}</p>
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
