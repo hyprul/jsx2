22f8771e941ee0944102a13bb8a2031875baf222
Sean Huang <hyprul@ucla.edu>
2019-11-27 16:16:16 -0600

    blah

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Sean";
+const lucky_number = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>Your lucky number is {lucky_number}</p>
+  </div>,
+  document.getElementById("root")
+);
